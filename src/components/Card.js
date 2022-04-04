5391b3ecff9de1fd16e6b0f7cb780b03aca0e692
Pranav Nanjappa <nanjappa.pranav@gmail.com>
2022-04-04 12:13:07 +0530

    Add status badge to experience list

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,21 @@ import React from "react";
 import Star from "../assets/star.png";
 
 const Card = (props) =>  {
+    let statusText;
+    if (props.openSpots === 0) {
+        statusText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        statusText = "ONLINE"
+    }    
     return (
         <div className="card" key={props.id}>
+            {statusText && <div className="card-status">{statusText}</div>}
             <img src={props.imageUrl} className="card-img"/>
             <div className="card-stats">
-            <div><img src={Star} className="card-star"/>   
-                {props.stats.rating}</div><br/>
-                <div>{props.stats.reviewCount} • </div>
-                <div>{props.location}</div>                     
+                <img src={Star} className="card-star"/>   
+                <span>{props.stats.rating}</span>
+                <span className="gray"> {`(${props.stats.reviewCount})`} • </span>
+                <span className="gray">{props.location}</span>                     
             </div>
             <p>{props.title}</p>
             <p><strong>From ${props.price}</strong> / person</p>            
